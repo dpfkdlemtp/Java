4621f03cb87bb66cc8dc5696e1da8dc170d783e7
dpfkdlemtp <56201106+dpfkdlemtp@users.noreply.github.com>
2020-12-03 15:39:13 +0900

    Create leetcode-PermutationInString.java

---
+++ week4/leetcode-PermutationInString.java
@@ -0,0 +1,35 @@
+/*
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1.
+In other words, one of the first string's permutations is the substring of the second string.
+*/
+class Solution {
+    static Map<Character, Integer> map=new HashMap();
+
+    public static void mapadd(Character c, Integer i){
+        
+        int val=map.getOrDefault(c,0)+i;
+        map.put(c,val);
+        if (val == 0) {
+            map.remove(c);
+        }
+    }
+    
+    public boolean checkInclusion(String s1, String s2) {
+        map.clear();
+        if(s1.length()>s2.length()) return false;
+        for (int i=0;i<s1.length();i++) {
+            mapadd(s1.charAt(i),1);
+            mapadd(s2.charAt(i),-1);
+        }
+        System.out.println(map);
+        if(map.size()==0) return true;
+        for(int i=s1.length();i<s2.length();i++){
+            mapadd(s2.charAt(i-s1.length()), 1);
+            mapadd(s2.charAt(i),-1);
+            System.out.println(map);
+            if(map.size()==0)   return true;
+        }
+
+        return false;
+    }
+}
