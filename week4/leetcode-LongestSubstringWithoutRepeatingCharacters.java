0d77033faf788cad00036c8e9010a17a48b2b8e9
dpfkdlemtp <56201106+dpfkdlemtp@users.noreply.github.com>
2020-12-07 04:59:53 +0900

    Update leetcode-LongestSubstringWithoutRepeatingCharacters.java

--- week4/leetcode-LongestSubstringWithoutRepeatingCharacters.java
+++ week4/leetcode-LongestSubstringWithoutRepeatingCharacters.java
@@ -8,33 +8,26 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 */
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        StringBuffer sb= new StringBuffer();
+       String s2= "";
         int ans=0;
         int count=1;
-        
+
         if(s.length()==0) return 0;
         else if(s.length()==1) return 1;
-        sb.append(s.substring(0,1));
 
-        for(int i=1;i<s.length();i++) {
-            String c=s.substring(i,i+1);
-            if(sb.indexOf(c)<0) {
-                count++;
+        for(int i=0;i<s.length();) {
+            char c=s.charAt(i);
+            if(!s2.contains(String.valueOf(c))) {
+                s2+=c;
+                i++;
             }
             else{
-                for(int j=i-1;j>=0;j--){
-                    if(s.substring(j,j+1).equals(c)){
-                        i=j+1;
-                        break;
-                    }
-                }
-                count=1;
-                sb.setLength(0);
-                c=s.substring(i,i+1);
+                count=s2.length();
+                s2=s2.substring(1);
             }
             if(count>ans) ans=count;
-            sb.append(c);
         }
+        count=s2.length();
         if(count>ans) ans=count;
         return ans;
     }
